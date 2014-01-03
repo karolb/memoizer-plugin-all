a159a8c34926655a7e09d43d615ce8002e533508
Karol <karol.banys@gmail.com>
2014-01-03 17:32:45 +0100

    Create project and add simple implementation of basic class.

---
+++ src/main/java/pl/edu/tcs/memoizer/plugins/impl/common/CommonMemeDownloader.java
@@ -0,0 +1,99 @@
+package pl.edu.tcs.memoizer.plugins.impl.common;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import pl.edu.uj.tcs.memoizer.plugins.EViewType;
+import pl.edu.uj.tcs.memoizer.plugins.IPluginFactory;
+import pl.edu.uj.tcs.memoizer.plugins.Meme;
+
+public class CommonMemeDownloader {
+	
+	/*
+	 * Get a page source, parse it,
+	 * extract memes and return
+	 */
+	static List<Meme> downloadMemesFromPage(URL url, EViewType viewType, IPluginFactory pluginFactory){
+		return extractMemesFromNodes(
+				extractMemeNodes(
+				downloadPageSource(url)), viewType, pluginFactory);
+	}
+	
+	/*
+	 * Download page source from specific url
+	 * If success returns downloaded page
+	 * otherwise returns null
+	 */
+	private static Document downloadPageSource(URL url){
+		try{
+			return Jsoup
+					.connect(url.toString())
+					.userAgent("Mozilla")
+					.get();
+		} catch(IOException e){}
+		return null;
+	}
+	
+	/*
+	 * Extract html meme-linked elements 
+	 * from given page source
+	 */
+	private static Elements extractMemeNodes(Document demotyPageSource){
+		//TODO
+		return null;
+	}
+	
+	/*
+	 * Parse html meme-linked element and extract meme info
+	 * returns list of parsed memes
+	 */
+	private static List<Meme> extractMemesFromNodes(Elements memeNodes, EViewType viewType, IPluginFactory pluginFactory){
+		List<Meme> lst = new ArrayList<Meme>();
+		
+		//TODO
+		
+		return lst;
+	}
+	
+	private static URL extractPageLinkFromATag(Element aTagElement){
+		try{
+			return new URL(/*host name +*/ aTagElement.attr("href"));
+		}catch(Exception e){}
+		return null;
+	}
+	
+	private static URL extractImageLinkFromImgTag(Element imgTagElement){
+		try{
+			return new URL(imgTagElement.attr("src"));
+		} catch(Exception e){}
+		return null;
+	}
+	
+	private static String extractTitleFromImgTag(Element imgTagElement){
+		try{
+			return imgTagElement.attr("alt");
+		} catch(Exception e){}
+		return "";
+	}
+	
+	private static int extractWidthFromImgTag(Element imgTagElement){
+		try{
+			return Integer.parseInt(imgTagElement.attr("width"));
+		} catch(Exception e){}
+		return 0;
+	}
+	
+	private static int extractHeightFromImgTag(Element imgTagElement){
+		try{
+			return Integer.parseInt(imgTagElement.attr("height"));
+		} catch(Exception e){}
+		return 0;
+	}
+}
